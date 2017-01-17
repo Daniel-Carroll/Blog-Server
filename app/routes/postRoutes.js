3457807d5b14f66c17a71ecea69d99eddf0453f5
DanielCarroll <danielcarroll95@gmail.com>
2017-01-17 16:02:56 -0600

    ehhh

--- app/routes/postRoutes.js
+++ app/routes/postRoutes.js
@@ -11,12 +11,7 @@ router.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
-    if (req.method === 'OPTIONS') {
-            res.statusCode = 204;
-            return res.end();
-        } else {
-            return next();
-        }
+   
     console.log('opening connection to /stuff/collections/Collection...')
     mongoose.connect('mongodb://cornbread:poop@ds139198.mlab.com:39198/blog')
     next(); //make sure next routes are ran
