fc66f48bb07631528086a2adb29f4e0bb24faddb
DanielCarroll <danielcarroll95@gmail.com>
2017-01-17 20:27:53 -0600

    L

--- app/routes/postRoutes.js
+++ app/routes/postRoutes.js
@@ -18,6 +18,9 @@ router.use(function(req, res, next){
 
 router.route('/')
     .options(function(req, res){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Cache-Control");
         res.end();
     })
 
